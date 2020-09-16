4ccd3cb7f0d04bbc724a2063b132e45181e885df
Patrick Hannan <patrick@hannan.com>
2020-09-15 23:41:19 -0400

    Made it so it displays name, temp, humidity, and wind speed.

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
 
-//Variables
   var cityHistory = [];
-  var city = $("#city-search").val()
-  var historyList = $("#history-list")
 
   if (localStorage.getItem("city") !== null){
     showHistory();
   }
-//Search Button Event Listener
+
   $("#search-btn").on("click", function (event) {
         event.preventDefault();
+        var city = $("#city-search").val();
         if (city !== "") {
             cityHistory.push(city);
             localStorage.setItem("city", JSON.stringify(cityHistory));
             showHistory();
+            showWeather(city);
             $("#city-search").val("")
         }
   })
-//Generates History List on the left side of the page
+
   function showHistory(){
         cityHistory = JSON.parse(localStorage.getItem("city"));
+        var historyList = $("#history-list");
         historyList.empty();
         for (var i = 0; i < cityHistory.length; i++) {
             var listItem = $("<li>").text(cityHistory[i])
@@ -29,8 +29,27 @@ $(document).ready(function () {
     }
   }
 
+  function showWeather(city){
+    var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8f7feb434e1136cc2c1ab8d74b64a2f6";
+    var latitude = []
+    var longitude = []
 
+    $("#filler").attr("class", "hide")
 
-
+    $.ajax({
+        url: queryURLWeather,
+        method: "GET",
+    }).then(function (response) {
+        $("#heading").text(response.name + " (" + moment().format("M/D/YYYY") + ")")
+        $("#icon").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+        $("#temperature").text("Temperature: " + response.main.temp + " °F");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+        $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
+        latitude.push(response.coord.lat)
+        longitude.push(response.coord.lon);
+        uvIndex(latitude, longitude);
+    })
+  }
+  
 
 })
