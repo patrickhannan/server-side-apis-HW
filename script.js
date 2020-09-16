5e70758a3ffee125ed2e4f17f8ddfa05ed7a75bb
Patrick Hannan <patrick@hannan.com>
2020-09-16 02:05:04 -0400

    cleaned the formatting of script.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-  var cityHistory = [];
+    var cityHistory = [];
 
-  if (localStorage.getItem("city") !== null){
+    if (localStorage.getItem("city") !== null){
     showHistory();
-  }
+    }
 
-  $("#search-btn").on("click", function (event) {
+    $("#search-btn").on("click", function (event) {
         event.preventDefault();
         var city = $("#city-search").val();
         if (city !== "") {
@@ -14,50 +14,47 @@ $(document).ready(function () {
             localStorage.setItem("city", JSON.stringify(cityHistory));
             showHistory();
             showWeather(city);
-            $("#city-search").val("")
+            $("#city-search").val("");
         }
-  })
+     })
 
-  $(".list-group-item").on("click", function () {
+    $(".list-group-item").on("click", function () {
         listItem = $(this).text();
         showWeather(listItem);
-  });
+    })
 
-  function showHistory(){
+    function showHistory(){
         cityHistory = JSON.parse(localStorage.getItem("city"));
         var historyList = $("#history-list");
         historyList.empty();
         for (var i = 0; i < cityHistory.length; i++) {
-            var listItem = $("<li>").text(cityHistory[i])
-            listItem.addClass("list-group-item")
+            var listItem = $("<li>").text(cityHistory[i]);
+            listItem.addClass("list-group-item");
             historyList.append(listItem);
+        }
     }
-  }
 
-  function showWeather(city){
-    var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=8f7feb434e1136cc2c1ab8d74b64a2f6";
-    var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=8f7feb434e1136cc2c1ab8d74b64a2f6";
-    var latitude = []
-    var longitude = []
+    function showWeather(city){
+        var queryURLWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=8f7feb434e1136cc2c1ab8d74b64a2f6";
+        var queryURLForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=8f7feb434e1136cc2c1ab8d74b64a2f6";
+        var latitude = []
+        var longitude = []
 
-   
+        $("#filler").attr("class", "hide")
 
-    $("#filler").attr("class", "hide")
-
-    $.ajax({
-        url: queryURLWeather,
-        method: "GET",
-    }).then(function (response) {
-        $("#heading").text(response.name + " (" + moment().format("M/D/YYYY") + ")")
-        $("#icon").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
-        $("#temperature").text("Temperature: " + response.main.temp + " °F");
-        $("#humidity").text("Humidity: " + response.main.humidity + "%");
-        $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
-        latitude.push(response.coord.lat)
-        longitude.push(response.coord.lon)
-        uvIndex(latitude, longitude);
-    })
-  
+        $.ajax({
+            url: queryURLWeather,
+            method: "GET",
+        }).then(function (response) {
+            $("#heading").text(response.name + " (" + moment().format("M/D/YYYY") + ")")
+            $("#icon").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+            $("#temperature").text("Temperature: " + response.main.temp + " °F");
+            $("#humidity").text("Humidity: " + response.main.humidity + "%");
+            $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
+            latitude.push(response.coord.lat)
+            longitude.push(response.coord.lon)
+            uvIndex(latitude, longitude);
+        })
   
 
     $.ajax({
@@ -77,8 +74,8 @@ $(document).ready(function () {
         $("#forecast-days").append(forecastCard);
         }
         
-    });
-    
+    })  
+
     function uvIndex(latitude, longitude) {
         var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?&lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=8f7feb434e1136cc2c1ab8d74b64a2f6";
 
@@ -93,7 +90,7 @@ $(document).ready(function () {
             uviRange.addClass("uv uv-favorable");
             uviRange.removeClass("uv-moderate");
             uviRange.removeClass("uv-severe");
-            
+        
         } else if (response.value >= 6 && response.value < 8) {
             uviRange.addClass("uv uv-moderate");
             uviRange.removeClass("uv-favorable");
@@ -105,8 +102,9 @@ $(document).ready(function () {
         }
 
         $("#current-weather").append(uviText, uviRange);
-      });
+        })
     }
 }
-
 })
+
+
