28d3801282c3a27e4d00eb84276dbd4d63ec7501
Patrick Hannan <patrick@hannan.com>
2020-09-15 22:35:55 -0400

    Added function that displays search history.

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 $(document).ready(function () {
 
+//Variables
   var cityHistory = [];
+  var city = $("#city-search").val()
+  var historyList = $("#history-list")
 
   if (localStorage.getItem("city") !== null){
     showHistory();
   }
-
-  $("#search-btn").on("click", function(){
-        preventDefault();
-        var city = $("#city-search").val();
+//Search Button Event Listener
+  $("#search-btn").on("click", function (event) {
+        event.preventDefault();
         if (city !== "") {
             cityHistory.push(city);
             localStorage.setItem("city", JSON.stringify(cityHistory));
             showHistory();
-            showWeather(city);
             $("#city-search").val("")
         }
   })
-
+//Generates History List on the left side of the page
   function showHistory(){
-        
+        cityHistory = JSON.parse(localStorage.getItem("city"));
+        historyList.empty();
+        for (var i = 0; i < cityHistory.length; i++) {
+            var listItem = $("<li>").text(cityHistory[i])
+            listItem.addClass("list-group-item")
+            historyList.append(listItem);
+    }
   }
 
 
